a0921514f1bc11ab2d3de6d030092b787404d0c3
Renat Gubaev <renat.gubaev@gmail.com>
2022-07-13 16:04:14 +0300

    auto commit

--- src/public-api.ts
+++ src/public-api.ts
@@ -5,13 +5,13 @@ export * from './IDestroyable';
 export * from './ILoadable';
 export * from './ISerializable';
 export * from './Loadable';
-
+//
 export * from './crypto/Ed25519';
 export * from './crypto/IKeyAsymmetric';
 export * from './crypto/ISignature';
 export * from './crypto/TweetNaCl';
 export * from './crypto/hash/Sha512';
-
+//
 export * from './dto/Filterable';
 export * from './dto/IArrayable';
 export * from './dto/IFilterable';
@@ -24,17 +24,17 @@ export * from './dto/IPaginationBookmark';
 export * from './dto/IUIDable';
 export * from './dto/Paginable';
 export * from './dto/PaginableBookmark';
-
+//
 export * from './error/ExtendedError';
 export * from './error/UnreachableStatementError';
-
+//
 export * from './executor/SequenceExecutor';
-
+//
 export * from './logger/ILogger';
 export * from './logger/Logger';
 export * from './logger/LoggerLevel';
 export * from './logger/LoggerWrapper';
-
+// 
 export * from './map/DestroyableMapCollection';
 export * from './map/FilterableMapCollection';
 export * from './map/MapCollection';
@@ -42,6 +42,56 @@ export * from './map/dataSource/DataSourceMapCollection';
 export * from './map/dataSource/FilterableDataSourceMapCollection';
 export * from './map/dataSource/PaginableBookmarkDataSourceMapCollection';
 export * from './map/dataSource/PaginableDataSourceMapCollection';
-
+// 
 export * from './observer/ObservableData';
+//
+export * from './promise/PromiseHandler';
+export * from './promise/PromiseReflector';
+//
+export * from './settings/AbstractSettingsStorage';
+//
+export * from './trace/ITraceable';
+export * from './trace/TraceUtil';
+//
+export * from './transport/AbstractTransportCommandHandler';
+export * from './transport/ITransport';
+export * from './transport/ITransportRequest';
+export * from './transport/ITransportResponse';
+export * from './transport/ITransportSettings';
+export * from './transport/Transport';
+export * from './transport/TransportCommand';
+export * from './transport/TransportCommandAsync';
+export * from './transport/TransportCommandAsyncHandler';
+export * from './transport/TransportCommandHandler';
+export * from './transport/TransportCommandOptions';
+export * from './transport/TransportEvent';
+export * from './transport/crypto/ITransportCryptoManager';
+export * from './transport/crypto/TransportCryptoManagerEd25519';
+export * from './transport/error/TransportInvalidDataError';
+export * from './transport/error/TransportNoConnectionError';
+export * from './transport/error/TransportTimeoutError';
+export * from './transport/error/TransportWaitError';
+export * from './transport/error/TransportWaitExceedError';
+export * from './transport/http/ITransportHttpRequest';
+export * from './transport/http/ITransportHttpSettings';
+export * from './transport/http/TransportHttp';
+export * from './transport/http/TransportHttpCommand';
+export * from './transport/http/TransportHttpCommandAsync';
+export * from './transport/http/TransportHttpCommandAsyncHandler';
+export * from './transport/http/TransportHttpCommandHandler';
+export * from './transport/local/TransportLocal';
+//
+export * from './util/ArrayUtil';
+export * from './util/CloneUtil';
+export * from './util/DateUtil';
+export * from './util/MathUtil';
+export * from './util/ObjectUtil';
+export * from './util/RandomUtil';
+export * from './util/StringUtil';
+export * from './util/TransformUtil';
+export * from './util/UrlUtil';
+export * from './util/ValidateUtil';
+//
+export * from './validate/ValidateResult';
+export * from './validate/validator/IsOneOfEnums';
 
