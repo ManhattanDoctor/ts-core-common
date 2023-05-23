c72a0717622eec15ff02358f681807684bfbf56c
Renat Gubaev <gubaev.r@n-t.io>
2023-05-23 14:55:01 +0300

    no message

---
+++ src/transport/TransportCommandHandlerAbstract.ts
@@ -0,0 +1,42 @@
+import { ExtendedError } from '../error';
+import { ILogger, LoggerWrapper } from '../logger';
+import { TransportWaitError } from './error/TransportWaitError';
+import { ITransport, ITransportCommand } from './ITransport';
+import * as _ from 'lodash';
+
+export abstract class TransportCommandHandlerAbstract<U, T extends ITransportCommand<U>> extends LoggerWrapper {
+    // --------------------------------------------------------------------------
+    //
+    //  Constructor
+    //
+    // --------------------------------------------------------------------------
+
+    protected constructor(logger: ILogger, protected transport: ITransport) {
+        super(logger);
+    }
+
+    // --------------------------------------------------------------------------
+    //
+    //  Protected Methods
+    //
+    // --------------------------------------------------------------------------
+
+    protected checkRequest(params: U): U {
+        return params;
+    }
+
+    protected handleError(command: T, error: Error): void {
+        if (TransportWaitError.instanceOf(error)) {
+            this.transport.wait(command);
+            return;
+        }
+        if (_.isNil(error)) {
+            error = new ExtendedError(`Undefined error`);
+        }
+        else if (_.isString(error)) {
+            error = new ExtendedError(error);
+        }
+        this.transport.complete(command, error);
+        this.error(error, error.stack);
+    }
+}
---
+++ src/transport/crypto/TransportCryptoManager.ts
@@ -0,0 +1,53 @@
+import { IKeyAsymmetric, ISignature } from "../../crypto";
+import { ObjectUtil, TransformUtil } from "../../util";
+import { ITransportCommand } from "../ITransport";
+import { ITransportCryptoManager } from "./ITransportCryptoManager";
+import * as _ from 'lodash';
+
+export abstract class TransportCryptoManager implements ITransportCryptoManager {
+    // --------------------------------------------------------------------------
+    //
+    //  Static Methods
+    //
+    // --------------------------------------------------------------------------
+
+    public static async sign<U>(command: ITransportCommand<U>, manager: ITransportCryptoManager, key: IKeyAsymmetric, nonce?: string): Promise<ISignature> {
+        return {
+            value: await manager.sign(command, nonce, key.privateKey),
+            publicKey: key.publicKey,
+            algorithm: manager.algorithm,
+            nonce: !_.isNil(nonce) ? nonce : Date.now().toString()
+        }
+    }
+
+    public static async verify<U>(command: ITransportCommand<U>, manager: ITransportCryptoManager, signature: ISignature): Promise<boolean> {
+        return manager.verify(command, signature);
+    }
+
+    // --------------------------------------------------------------------------
+    //
+    //  Abstract  Methods
+    //
+    // --------------------------------------------------------------------------
+
+    abstract sign<U>(command: ITransportCommand<U>, nonce: string, privateKey: string): Promise<string>;
+    
+    abstract verify<U>(command: ITransportCommand<U>, signature: ISignature): Promise<boolean>;
+    
+    abstract readonly algorithm: string;
+
+    // --------------------------------------------------------------------------
+    //
+    //  Protected Methods
+    //
+    // --------------------------------------------------------------------------
+
+    protected toString<U>(command: ITransportCommand<U>, nonce: string): string {
+        let request = !_.isNil(command.request) ? this.toStringRequest(command.request) : '';
+        return `${command.name}${request}${nonce}`;
+    }
+
+    protected toStringRequest<U>(item: U): string {
+        return _.isObject(item) ? TransformUtil.fromJSON(ObjectUtil.sortKeys(item, true)) : item.toString();
+    }
+}
