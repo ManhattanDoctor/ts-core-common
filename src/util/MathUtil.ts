785591c7d12dd58b48b8cde1bd5b19714e1c26c1
Renat Gubaev <gubaev.r@n-t.io>
2025-04-22 12:58:05 +0300

    auto commit

--- src/util/MathUtil.ts
+++ src/util/MathUtil.ts
@@ -85,7 +85,7 @@ export class MathUtil {
         return MathUtil.toString(MathUtil.new(value).pow(MathUtil.new(n)));
     }
 
-    public modulo(first: string, second: string): string {
+    public static modulo(first: string, second: string): string {
         if (MathUtil.isInvalid(first) || MathUtil.isInvalid(second)) {
             return null;
         }
